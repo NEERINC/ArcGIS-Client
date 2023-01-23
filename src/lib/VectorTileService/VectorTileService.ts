63603cd6973b47e4e897774ba2cc557c07c225a9
Matthew Downs <matthew@neer.ai>
2023-01-22 19:39:47 -0600

    Update vector tile service

--- src/lib/VectorTileService/VectorTileService.ts
+++ src/lib/VectorTileService/VectorTileService.ts
@@ -1,14 +1,42 @@
+import { IFeatureServiceDefinition } from '@esri/arcgis-rest-feature-service'
 import { ArcGISIdentityManager } from '@esri/arcgis-rest-request'
-import { ServerType } from '../../types'
+import {
+  LayerDefinition,
+  ServerType
+} from '../../types'
 import Service from '../Service'
 
-class VectorTileService extends Service<ServerType.MapServer> {
-  constructor(url: string, identityManager?: ArcGISIdentityManager) {
-    super(ServerType.MapServer, url, identityManager)
+class VectorTileService extends Service<ServerType.VectorTileServer> {
+  public readonly definition: IFeatureServiceDefinition
+  public readonly layers: LayerDefinition[]
+
+  public get tile() { return `${this.url}/tile/{z}/{y}/{x}.pbf` }
+
+  constructor(definition: IFeatureServiceDefinition, layers: LayerDefinition[], url: string, identityManager?: ArcGISIdentityManager, cache?: boolean) {
+    super(ServerType.VectorTileServer, url, identityManager)
+    this.definition = definition
+    this.layers = layers
   }
 
   public static async load(url: string, identityManager?: ArcGISIdentityManager | undefined) {
-    return new VectorTileService(url, identityManager)
+    const params = new URLSearchParams({
+      f: 'json'
+    })
+    if (identityManager != null) params.append('token', identityManager.token)
+    const response = await fetch(`${url}?${params.toString()}`, {
+      method: 'GET'
+    })
+    const definition = await response.json() as IFeatureServiceDefinition
+
+    const layers = await Promise.all(definition.layers.map(layer => layer.id).filter(id => id != null).map(async id => {
+      const layerResponse = await fetch(`${url}/${id!}?${params.toString()}`, {
+        method: 'GET'
+      })
+      const layerDefinition = await layerResponse.json()
+      return layerDefinition as LayerDefinition
+    }))
+
+    return new VectorTileService(definition, layers, url, identityManager)
   }
 }
 
