e3db3eb023817a36bca26025fb5e8daa6cb754bd
Matthew Downs <matthew@neer.ai>
2023-01-22 20:13:39 -0600

    Remove layers from vector tile service

--- src/lib/VectorTileService/VectorTileService.ts
+++ src/lib/VectorTileService/VectorTileService.ts
@@ -8,14 +8,12 @@ import Service from '../Service'
 
 class VectorTileService extends Service<ServerType.VectorTileServer> {
   public readonly definition: IFeatureServiceDefinition
-  public readonly layers: LayerDefinition[]
 
   public get tile() { return `${this.url}/tile/{z}/{y}/{x}.pbf` }
 
-  constructor(definition: IFeatureServiceDefinition, layers: LayerDefinition[], url: string, identityManager?: ArcGISIdentityManager, cache?: boolean) {
+  constructor(definition: IFeatureServiceDefinition, url: string, identityManager?: ArcGISIdentityManager, cache?: boolean) {
     super(ServerType.VectorTileServer, url, identityManager)
     this.definition = definition
-    this.layers = layers
   }
 
   public static async load(url: string, identityManager?: ArcGISIdentityManager | undefined) {
@@ -28,15 +26,7 @@ class VectorTileService extends Service<ServerType.VectorTileServer> {
     })
     const definition = await response.json() as IFeatureServiceDefinition
 
-    const layers = await Promise.all(definition.layers.map(layer => layer.id).filter(id => id != null).map(async id => {
-      const layerResponse = await fetch(`${url}/${id!}?${params.toString()}`, {
-        method: 'GET'
-      })
-      const layerDefinition = await layerResponse.json()
-      return layerDefinition as LayerDefinition
-    }))
-
-    return new VectorTileService(definition, layers, url, identityManager)
+    return new VectorTileService(definition, url, identityManager)
   }
 }
 
